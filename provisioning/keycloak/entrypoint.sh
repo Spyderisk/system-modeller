13dfbed85e5f2e1f8b223bc5049744e788dcfd66
Stephen C Phillips <steve@scphillips.com>
2023-04-04 11:11:36 +0100

    Adds debugging to entrypoint script

--- provisioning/keycloak/entrypoint.sh
+++ provisioning/keycloak/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+whoami
+ls -ld /opt/keycloak/data/import/
+ls -l /opt/keycloak/data/import/
 sed -e s/KEYCLOAK_CREDENTIALS_SECRET/${KEYCLOAK_CREDENTIALS_SECRET}/ /opt/keycloak/data/import/ssm.template > /opt/keycloak/data/import/ssm.json
 /opt/keycloak/bin/kc.sh start-dev --import-realm --http-relative-path /auth
