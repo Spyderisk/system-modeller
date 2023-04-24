c7df9849860f6c4cbd53c7cddf762c950c851ba2
kem <kem@it-innovation.soton.ac.uk>
2023-04-24 11:41:07 +0100

    #10: Modify InitializeManagementGraph to automatically check new "kbdata" folder for domain bundles at start-up (when resetOnStart is true, etc). All located zipfiles are then extracted and imported to SSM.

--- src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/InitializeManagementGraph.java
+++ src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/InitializeManagementGraph.java
@@ -24,24 +24,23 @@
 /////////////////////////////////////////////////////////////////////////
 package uk.ac.soton.itinnovation.security.systemmodeller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.io.IOUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uk.ac.soton.itinnovation.security.systemmodeller.auth.KeycloakAdminClient;
 import uk.ac.soton.itinnovation.security.systemmodeller.semantics.ModelObjectsHelper;
 import uk.ac.soton.itinnovation.security.systemmodeller.semantics.StoreModelManager;
+import uk.ac.soton.itinnovation.security.systemmodeller.util.DomainModelUtils;
 import uk.ac.soton.itinnovation.security.systemmodeller.util.PaletteGenerator;
 
 /**
@@ -69,8 +68,8 @@ public class InitializeManagementGraph implements CommandLineRunner {
 
 		//logger.info("Getting list of domain models from ontologies.json");
 
-		ArrayList<String> ontologyNames = new ArrayList();
-		ArrayList<String> defaultUserOntologies = new ArrayList();
+		ArrayList<String> ontologyNames = new ArrayList<>();
+		ArrayList<String> defaultUserOntologies = new ArrayList<>();
 
 		/*
 		try {
@@ -140,11 +139,91 @@ public class InitializeManagementGraph implements CommandLineRunner {
 			}
 			*/
 			
+			logger.info("Checking for kbdata folder...");
+	
+			//List of identified zip files located in kbdata folder
+			ArrayList<File> zipfiles = new ArrayList<>();
+	
+			try {
+				File kbDataDir = new File("/code/kbdata");
+				if (kbDataDir.isDirectory()) {
+					File[] fileList = kbDataDir.listFiles();
+					logger.info("Located .zip files: ");
+					if (fileList != null) {
+						for (File file : fileList) {
+							if (!file.isDirectory()) {
+								String filename = file.getName();
+								if (filename.endsWith(".zip")) {
+									logger.info(filename);
+									zipfiles.add(file);
+								}
+							}
+						}
+					}
+				}
+				else {
+					logger.info("Folder is not a directory: ", kbDataDir);
+				}
+	
+				DomainModelUtils domainModelUtils= new DomainModelUtils();
+		
+				for (File file : zipfiles) {
+					Map<String, String> results = domainModelUtils.extractDomainBundle(file, true, null, null);
+
+					String domainUri = null;
+					String domainModelName = null;
+					File iconMappingFile = null;
+					String nqFilepath = null;
+
+					if (results.containsKey("domainUri")) {
+						domainUri = results.get("domainUri");
+					}
+		
+					if (results.containsKey("domainModelName")) {
+						domainModelName = results.get("domainModelName");
+					}
+		
+					if (results.containsKey("nqFilepath")) {
+						File f = new File(results.get("nqFilepath"));
+						nqFilepath = f.getAbsolutePath();
+					}
+					
+					if (results.containsKey("iconMappingFile")) {
+						iconMappingFile = new File(results.get("iconMappingFile"));
+					}
+		
+					logger.debug("domainUri: {}", domainUri);
+					logger.info("domainModelName: {}", domainModelName);
+					logger.debug("domain model file: {}", nqFilepath);
+
+					logger.info("Adding domain model {} to the management graph", domainModelName);
+					storeModelManager.loadModel(domainModelName, domainUri, nqFilepath);
+
+					//Create palette using icon mappings file
+					boolean paletteCreated = false;
+
+					if (iconMappingFile != null) {
+						logger.debug("Loading icon mappings from file: {}", iconMappingFile.getAbsolutePath());
+						paletteCreated = PaletteGenerator.createPalette(domainUri, modelObjectsHelper, new FileInputStream(iconMappingFile));
+					}
+
+					if (!paletteCreated) {
+						logger.error("Palette not generated for: " + domainUri);
+					}
+					
+				}
+	
+			}
+			catch (IOException ex) {
+				logger.error("Could not load domain models", ex);
+				System.exit(1);
+			}
+			
 			logger.debug("Domain models in management graph: {}", storeModelManager.getDomainModels());
 
 			logger.info("Putting all users in the management graph");
 			List<UserRepresentation> users = keycloakAdminClient.getAllUsers();
-			ArrayList<String> userNames = new ArrayList();
+			ArrayList<String> userNames = new ArrayList<>();
 
 			for (UserRepresentation u : users){
 				logger.debug("{}", u.getUsername());
