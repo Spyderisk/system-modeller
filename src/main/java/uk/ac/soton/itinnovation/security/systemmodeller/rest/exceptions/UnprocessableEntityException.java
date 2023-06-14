b567a524e4908cd408c38c1098222e71788d3d9a
kem <kem@it-innovation.soton.ac.uk>
2023-06-14 10:59:41 +0100

    #16: Create UnprocessableEntityException to support HTTP 422 errors

---
+++ src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/rest/exceptions/UnprocessableEntityException.java
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// © University of Southampton IT Innovation Centre, 2023
+//
+// Copyright in this software belongs to University of Southampton
+// IT Innovation Centre of Gamma House, Enterprise Road,
+// Chilworth Science Park, Southampton, SO16 7NS, UK.
+//
+// This software may not be used, sold, licensed, transferred, copied
+// or reproduced in whole or in part in any manner or form or in or
+// on any media by any person other than in accordance with the terms
+// of the Licence Agreement supplied with the software, or otherwise
+// without the prior written consent of the copyright owners.
+//
+// This software is distributed WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE, except where stated in the Licence Agreement supplied with
+// the software.
+//
+//      Created By :            Ken Meacham
+//      Created Date :          2023-06-13
+//      Created for Project :   CyberKit4SME
+//
+/////////////////////////////////////////////////////////////////////////
+package uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Generic exception for any UNPROCESSABLE_ENTITY error that needs a custom message
+ * Will present as an HTTP response.
+ */
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class UnprocessableEntityException extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+} 
