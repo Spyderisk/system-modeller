007dcb148cf41a0f3284bc2c4368396513610440
kem <kem@it-innovation.soton.ac.uk>
2023-11-17 17:57:25 +0000

    #93: Add link to OpenAPI docs from Help menu

--- src/main/webapp/app/common/documentation/documentation.js
+++ src/main/webapp/app/common/documentation/documentation.js
@@ -2,3 +2,8 @@ export function openDocumentation(e, link) {
     e.stopPropagation();
     window.open("/documentation/" + link, "system-modeller-docs");
 }
+
+export function openApiDocs(e) {
+    e.stopPropagation();
+    window.open("/system-modeller/swagger-ui.html", "openapi-docs");
+}
