49556e4ef0afaef45392fddca6cfe5a4a874f123
panos <pm@it-innovation.soton.ac.uk>
2023-11-22 10:34:43 +0000

    #67 adding recommendation DTO models

---
+++ src/main/java/uk/ac/soton/itinnovation/security/modelvalidator/attackpath/dto/RecommendationReportDTO.java
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// © University of Southampton IT Innovation Centre, 2023
+//
+// Copyright in this software belongs to University of Southampton
+// IT Innovation Centre of Gamma House, Enterprise Road,
+// Chilworth Science Park, Southampton, SO16 7NS, UK.
+//
+// This software may not be used, sold, licensed, transferred, copied
+// or reproduced in whole or in part in any manner or form or in or
+// on any media by any person other than in accordance with the terms
+// of the Licence Agreement supplied with the software, or otherwise
+// without the prior written consent of the copyright owners.
+//
+// This software is distributed WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE, except where stated in the Licence Agreement supplied with
+// the software.
+//
+//      Created By:				Panos Melas
+//      Created Date:			2023-11-14
+//      Created for Project :   Cyberkit4SME
+//
+/////////////////////////////////////////////////////////////////////////
+package uk.ac.soton.itinnovation.security.modelvalidator.attackpath.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class RecommendationReportDTO {
+    private StateDTO current;
+    private List<RecommendationDTO> recommendations;
+}
