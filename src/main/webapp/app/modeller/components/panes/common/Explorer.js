702d8e4a36f7f68c4739cc5a9ba679209dde10d3
kem <kem@it-innovation.soton.ac.uk>
2024-01-29 12:29:34 +0000

    #128: Refactor RecommendationsExplorer to use common Explorer component. TODO: need to also refactor other explorer classes to use the Explorer component.

---
+++ src/main/webapp/app/modeller/components/panes/common/Explorer.js
@@ -0,0 +1,125 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {Rnd} from "../../../../../node_modules/react-rnd/lib/index";
+import {bringToFrontWindow} from "../../../actions/ViewActions";
+import {openDocumentation} from "../../../../common/documentation/documentation"
+
+class Explorer extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.renderRnd = this.renderRnd.bind(this);
+
+        this.state = {
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        let shouldComponentUpdate = true;
+
+        if ((!this.props.show) && (!nextProps.show)) {
+            return false;
+        }
+
+        if (nextProps.loading.model) {
+            return false;
+        }
+
+        if (this.props.isActive != nextProps.isActive) {
+            return true;
+        }
+
+        if (this.props.selectedAsset != nextProps.selectedAsset) {
+            return true;
+        }
+
+        return shouldComponentUpdate;
+    }
+
+    renderRnd() {
+        let {xScale, width, height} = this.props.rndParams
+
+        return (
+            <Rnd bounds={ '#view-boundary' }
+                 default={{
+                     x: window.outerWidth * xScale,
+                     y: (100 / window.innerHeight) * window.devicePixelRatio,
+                     width: width,
+                     height: height,
+                 }}
+                 style={{ zIndex: this.props.windowOrder }}
+                 minWidth={150}
+                 minHeight={200}
+                 cancel={".content, .text-primary, strong, span"}
+                 onResize={(e) => {
+                     if (e.stopPropagation) e.stopPropagation();
+                     if (e.preventDefault) e.preventDefault();
+                     e.cancelBubble = true;
+                     e.returnValue = false;
+                 }}
+                 onDrag={(e) => {
+                     if (e.stopPropagation) e.stopPropagation();
+                     if (e.preventDefault) e.preventDefault();
+                     e.cancelBubble = true;
+                     e.returnValue = false;
+                 }}
+                 className={!this.props.show ? "hidden" : null}>
+                 <div className="explorer">
+                      <button className="header"
+                          onMouseDown={() => {
+                              this.props.dispatch(bringToFrontWindow(this.props.windowName));
+                          }}
+                      >
+                          <div className="header header-no-padding">
+                              <h1>
+                                  <div className={"doc-help-explorer"}>
+                                      <div className="title">
+                                          {this.props.title}
+                                      </div>
+                                  </div>
+                              </h1>
+                              <span className="button">
+                                  <button onClick={e => this.props.onHide()}>
+                                      <i className="fa fa-times"/>
+                                  </button>
+                              </span>
+                              <span className="button">
+                                  <button onClick={e => openDocumentation(e, this.props.documentationLink)}>
+                                      <i className="fa fa-question"/>
+                                  </button>
+                              </span>
+                          </div>
+                      </button>
+
+                      {this.props.renderContent()}
+                 </div>
+            </Rnd>
+        );
+    }
+    
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+
+        return (this.renderRnd())
+    }   
+}
+
+Explorer.propTypes = {
+    title: PropTypes.string,
+    windowName: PropTypes.string,
+    documentationLink: PropTypes.string,
+    rndParams: PropTypes.object,
+    selectedAsset: PropTypes.object,
+    isActive: PropTypes.bool, // is in front of other panels
+    show: PropTypes.bool,
+    onHide: PropTypes.func,
+    loading: PropTypes.object,
+    dispatch: PropTypes.func,
+    renderContent: PropTypes.func,
+    windowOrder: PropTypes.number,
+};
+
+export default Explorer;
--- src/main/webapp/app/modeller/components/panes/recommendationsExplorer/RecommendationsExplorer.js
+++ src/main/webapp/app/modeller/components/panes/recommendationsExplorer/RecommendationsExplorer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import {Rnd} from "../../../../../node_modules/react-rnd/lib/index";
-import {bringToFrontWindow} from "../../../actions/ViewActions";
 import {connect} from "react-redux";
-import {openDocumentation} from "../../../../common/documentation/documentation"
 import {JsonView, defaultStyles} from 'react-json-view-lite';
 import 'react-json-view-lite/dist/index.css';
+import Explorer from "../common/Explorer"
 
 class RecommendationsExplorer extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.renderContent = this.renderContent.bind(this);
 
         this.state = {
         }
@@ -43,69 +43,38 @@ class RecommendationsExplorer extends React.Component {
             return null;
         }
 
-        let recommendations = this.props.recommendations;
-
+        let rndParams = {
+            xScale: 0.20,
+            width: 700,
+            height: 600
+        }
+        
         return (
-          <Rnd bounds={ '#view-boundary' }
-               default={{
-                   x: window.outerWidth * 0.20,
-                   y: (100 / window.innerHeight) * window.devicePixelRatio,
-                   width: 700,
-                   height: 600,
-               }}
-               style={{ zIndex: this.props.windowOrder }}
-               minWidth={150}
-               minHeight={200}
-               cancel={".content, .text-primary, strong, span"}
-               onResize={(e) => {
-                   if (e.stopPropagation) e.stopPropagation();
-                   if (e.preventDefault) e.preventDefault();
-                   e.cancelBubble = true;
-                   e.returnValue = false;
-               }}
-               onDrag={(e) => {
-                   if (e.stopPropagation) e.stopPropagation();
-                   if (e.preventDefault) e.preventDefault();
-                   e.cancelBubble = true;
-                   e.returnValue = false;
-               }}
-               className={!this.props.show ? "hidden" : null}>
-               <div className="recommendations-explorer">
-                    <button className="header"
-                        onMouseDown={() => {
-                            this.props.dispatch(bringToFrontWindow("recommendationsExplorer"));
-                        }}
-                    >
-                        <div className="header header-no-padding">
-                            <h1>
-                                <div className={"doc-help-explorer"}>
-                                    <div className="title">
-                                        {"Recommendations Explorer"}
-                                    </div>
-                                </div>
-                            </h1>
-                            <span classID="recommendations-close"
-                                  className="button">
-                                <button onClick={e => this.props.onHide()}>
-                                    <i className="fa fa-times"/>
-                                </button>
-                            </span>
-                            <span className="button">
-                                <button onClick={e => openDocumentation(e, "redirect/recommendations-explorer")}>
-                                    <i className="fa fa-question"/>
-                                </button>
-                            </span>
-                        </div>
-                    </button>
+            <Explorer
+                title={"Recommendations Explorer"}
+                windowName={"recommendationsExplorer"}
+                documentationLink={"redirect/recommendations-explorer"}
+                rndParams={rndParams}
+                selectedAsset={this.props.selectedAsset}
+                isActive={this.props.isActive}
+                show={this.props.show}
+                onHide={this.props.onHide}
+                loading={this.props.loading}
+                dispatch={this.props.dispatch}
+                renderContent={this.renderContent}
+                windowOrder={this.props.windowOrder}
+            />
+        )
+    }
 
-                    <div className="content">
-                        {recommendations && <JsonView data={recommendations} shouldExpandNode={shouldExpandRecommendationsNode} style={defaultStyles} />}
-                   </div>
-               </div>
-
-          </Rnd>
-        );
-    }   
+    renderContent() {
+        let recommendations = this.props.recommendations;
+        return (
+            <div className="content">
+                {recommendations && <JsonView data={recommendations} shouldExpandNode={shouldExpandRecommendationsNode} style={defaultStyles} />}
+            </div>
+        )
+    }
 }
 
 function shouldExpandRecommendationsNode(level) {
@@ -120,7 +89,7 @@ RecommendationsExplorer.propTypes = {
     onHide: PropTypes.func,
     loading: PropTypes.object,
     dispatch: PropTypes.func,
-    windowOrder: PropTypes.object,
+    windowOrder: PropTypes.number,
 };
 
 let mapStateToProps = function (state) {
