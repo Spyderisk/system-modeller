5ce2b01227a0af080ba0e7533004eebdcf79c3f7
kem <kem@it-innovation.soton.ac.uk>
2024-01-31 11:13:24 +0000

    #67: Tidy imports

--- src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/rest/AsyncController.java
+++ src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/rest/AsyncController.java
@@ -1,94 +1,30 @@
 
 package uk.ac.soton.itinnovation.security.systemmodeller.rest;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.rmi.UnexpectedException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-import javax.naming.SizeLimitExceededException;
-import javax.servlet.http.HttpServletRequest;
-
 import java.net.URI;
-import java.util.UUID;
-
-import org.keycloak.representations.idm.UserRepresentation;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import uk.ac.soton.itinnovation.security.model.system.RiskCalculationMode;
-import uk.ac.soton.itinnovation.security.model.system.RiskLevelCount;
-import uk.ac.soton.itinnovation.security.model.system.RiskVector;
 import uk.ac.soton.itinnovation.security.modelquerier.JenaQuerierDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.AssetDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.ControlDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.ControlSetDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.ControlStrategyDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.LevelDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.MisbehaviourDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.MisbehaviourSetDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.RiskCalcResultsDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.ThreatDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.TrustworthinessAttributeDB;
-import uk.ac.soton.itinnovation.security.modelquerier.dto.TrustworthinessAttributeSetDB;
-import uk.ac.soton.itinnovation.security.modelvalidator.ModelValidator;
 import uk.ac.soton.itinnovation.security.modelvalidator.Progress;
-import uk.ac.soton.itinnovation.security.modelvalidator.attackpath.AttackPathAlgorithm;
-import uk.ac.soton.itinnovation.security.modelvalidator.attackpath.RecommendationsAlgorithm;
 import uk.ac.soton.itinnovation.security.modelvalidator.attackpath.RecommendationsAlgorithmConfig;
 import uk.ac.soton.itinnovation.security.modelvalidator.attackpath.dto.RecommendationReportDTO;
-import uk.ac.soton.itinnovation.security.modelvalidator.attackpath.dto.TreeJsonDoc;
 import uk.ac.soton.itinnovation.security.semanticstore.AStoreWrapper;
-import uk.ac.soton.itinnovation.security.semanticstore.IStoreWrapper;
 import uk.ac.soton.itinnovation.security.semanticstore.JenaTDBStoreWrapper;
-import uk.ac.soton.itinnovation.security.semanticstore.util.SparqlHelper;
 import uk.ac.soton.itinnovation.security.systemmodeller.attackpath.AsyncService.RecStatus;
 import uk.ac.soton.itinnovation.security.systemmodeller.attackpath.AsyncService;
 import uk.ac.soton.itinnovation.security.systemmodeller.auth.KeycloakAdminClient;
@@ -96,27 +32,11 @@ import uk.ac.soton.itinnovation.security.systemmodeller.model.Model;
 import uk.ac.soton.itinnovation.security.systemmodeller.model.ModelFactory;
 import uk.ac.soton.itinnovation.security.systemmodeller.model.WebKeyRole;
 import uk.ac.soton.itinnovation.security.systemmodeller.mongodb.IModelRepository;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.dto.LoadingProgress;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.dto.LoadingProgressResponse;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.dto.ModelDTO;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.dto.UpdateModelResponse;
 import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.BadRequestErrorException;
 import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.InternalServerErrorException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.MisbehaviourSetInvalidException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.ModelException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.ModelInvalidException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.NotAcceptableErrorException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.NotFoundErrorException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.UnprocessableEntityException;
-import uk.ac.soton.itinnovation.security.systemmodeller.rest.exceptions.UserForbiddenFromDomainException;
 import uk.ac.soton.itinnovation.security.systemmodeller.semantics.ModelObjectsHelper;
 import uk.ac.soton.itinnovation.security.systemmodeller.semantics.StoreModelManager;
-import uk.ac.soton.itinnovation.security.systemmodeller.util.ReportGenerator;
 import uk.ac.soton.itinnovation.security.systemmodeller.util.SecureUrlHelper;
-
-import java.util.concurrent.CompletionStage;
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/async")
