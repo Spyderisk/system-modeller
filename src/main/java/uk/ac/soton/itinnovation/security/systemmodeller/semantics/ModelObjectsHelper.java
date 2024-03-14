5fb8abda73b5c44f518234ed9645e8273f1d9745
kem <kem@it-innovation.soton.ac.uk>
2024-03-14 12:05:34 +0000

    #145: Remove cancelTask, which was unused

--- src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/semantics/ModelObjectsHelper.java
+++ src/main/java/uk/ac/soton/itinnovation/security/systemmodeller/semantics/ModelObjectsHelper.java
@@ -331,18 +331,6 @@ public class ModelObjectsHelper {
 		assetUris.remove(id);
 	}
 
-    public boolean cancelTask(String mId) {
-		if (taskFutures.containsKey(mId)){
-			ScheduledFuture<?> taskExecution = taskFutures.get(mId);
-			if (!taskExecution.isDone()) {
-                boolean mayInterruptIfRunning = true;
-                boolean wasCancelled = taskExecution.cancel(mayInterruptIfRunning);
-                return wasCancelled;
-            }
-        }
-        return false;
-    }
-
 	public boolean registerTaskExecution(String modelId, ScheduledFuture<?> future) {
 
 		if (taskFutures.containsKey(modelId)){
