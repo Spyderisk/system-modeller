35854ea7eb73e0428bd6dac8df857bb2f15ea274
kem <kem@it-innovation.soton.ac.uk>
2024-06-26 16:35:25 +0100

    #68: Also add about reducer to modeller and domainManager page

--- src/main/webapp/app/domainManager/reducers/index.js
+++ src/main/webapp/app/domainManager/reducers/index.js
@@ -1,11 +1,12 @@
 import {combineReducers} from 'redux';
 import auth from "../../common/reducers/auth";
+import about from "../../common/reducers/about";
 import {domainManager} from "./domainManager";
-
 
 const reducers = combineReducers({
     domainManager,
-    auth
+    auth,
+    about,
     //form: reduxFormReducer, // this has to be named form for reduxForm to work
 });
 
